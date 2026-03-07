d7f0bbeca1e23a72dc4fab815a76f45ce78cf51f
Kayes Mahmud <mahmudkayes30@gmail.com>
2026-03-07 12:38:34 +0600

    Added the 3 buttons functionalities

--- scripts/home.js
+++ scripts/home.js
@@ -2,6 +2,25 @@ const allIssues = [];
 const openIssues = [];
 const closedIssues = [];
 
+const searchInput = document.getElementById("search-input");
+const issueCountDisplay = document.getElementById("issues-count");
+const issueContainer = document.getElementById("issue-card-container");
+
+const allBtn = document.getElementById("all-btn");
+const openBtn = document.getElementById("open-btn");
+const closedBtn = document.getElementById("closed-btn");
+
+const createLabels = (arr) => {
+  // console.log(arr);
+
+  const htmlElements = arr.map(
+    (element) =>
+      `<button class="p-1 rounded-md font-semibold border-2 border-yellow-400">${element}<button/>`,
+  );
+
+  return htmlElements.join(" ");
+};
+
 const loadAllIssue = () => {
   const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
   fetch(url)
@@ -18,8 +37,70 @@ const individualDataLoad = (issues) => {
     } else if (issue.status == "closed") closedIssues.push(issue);
   });
   console.log(allIssues.length);
+  renderAll(allIssues);
+  allBtn.classList.add("active-btn");
 };
+
+const renderAll = (issues) => {
+  issueCountDisplay.innerText = issues.length;
+
+  issueContainer.innerHTML = "";
+  issues.forEach((issue) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <div
+        class="h-full card shadow-sm border border-gray-200 p-3 border-t-4 space-y-2 ${issue.status == "open" ? "border-t-green-600" : "border-t-violet-800"}"
+      >
+        <div class="relative py-4">
+          <p
+            class="text-center w-20 bg-red-100 rounded-full text-red-400 font-semibold absolute top-0 right-0"
+          >
+            ${issue.priority}
+          </p>
+        </div>
+
+        <h2 class="text-[14px] font-bold">
+         ${issue.title}
+        </h2>
+        <p class="text-[#64748B]">
+          ${issue.description}
+        </p>
+        <div class="flex flex-wrap gap-1">
+          ${createLabels(issue.labels)}
+        </div>
+
+        <p class="text-[#64748B]">#${issue.author}</p>
+        <p class="text-[#64748B]">${new Date(
+          issue.createdAt,
+        ).toDateString()}</p>
+      </div>
+    `;
+    issueContainer.append(div);
+  });
+};
+
+allBtn.addEventListener("click", () => {
+  allBtn.classList.add("active-btn");
+  openBtn.classList.remove("active-btn");
+  closedBtn.classList.remove("active-btn");
+  renderAll(allIssues);
+});
+openBtn.addEventListener("click", () => {
+  allBtn.classList.remove("active-btn");
+  openBtn.classList.add("active-btn");
+  closedBtn.classList.remove("active-btn");
+  renderAll(openIssues);
+});
+closedBtn.addEventListener("click", () => {
+  allBtn.classList.remove("active-btn");
+  openBtn.classList.remove("active-btn");
+  closedBtn.classList.add("active-btn");
+  renderAll(closedIssues);
+});
 
 loadAllIssue();
 
-// console.log(allIssues.length);
+//----------------------------------------------------------------
+searchInput.addEventListener("input", () => {
+  console.log(searchInput.value);
+});
